51df3e5e6455a6aff38b9066b0caf4915b255e8a
siddikur-dev <siddikur.dev@gmail.com>
2025-07-30 18:12:00 +0600

    for/3 solved

--- for/3 sum of odd numbers.js
+++ for/3 sum of odd numbers.js
@@ -1,18 +1,18 @@
+// Display sum of all the odd numbers from 91 to 129.
+let sumOdd = 0;
+for (let i = 91; i <= 129; i += 2) {
+  console.log(i);
+  sumOdd += i;
+  console.log("sumOdd all number:", sumOdd);
+}
 
+// Display sum of all the even numbers from 51 to 85.
 
-/***
-
-Subtask-1:
-
-Display sum of all the odd numbers from 91 to 129.
-
- */
-/***
-
-Subtask-2:
-
-Display sum of all the even numbers from 51 to 85.
-
- */
-
-/*programming hero*/
+let sumEven = 0;
+for (let i = 51; i <= 85; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+    sumEven += i;
+    console.log("sumEven all number:", sumEven);git 
+  }
+}
