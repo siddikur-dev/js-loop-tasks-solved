e018658067710521680296efa09eb9b79c9323c5
siddikur-dev <siddikur.dev@gmail.com>
2025-07-30 18:19:09 +0600

    for/4 solved

--- for/4 multiplication table.js
+++ for/4 multiplication table.js
@@ -1,8 +1,9 @@
-/***
+// Generate a multiplication table for number 9
 
-Generate a multiplication table for number 9
+// for (let i = 1; i * 9; i++) {
+//   console.log(i);
+// }
 
- */
-
-
-/*programming hero*/
+for (let i = 1; i <= 10; i++) {
+  console.log(i * 9);
+}
