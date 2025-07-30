f3bf2e35aec4c73729d36f65ab7a6123b17721e7
siddikur-dev <siddikur.dev@gmail.com>
2025-07-30 17:01:43 +0600

    break/sum added

--- break/1 century.js
+++ break/1 century.js
@@ -1,5 +1,5 @@
 // Write a loop 1 to 200. Use break to exit the loop once you find 100.
-
+// SOLVED
 for (let number = 1; number < 200; number++) {
   if (number === 101) {
     break;
--- break/2 sum upto 100.js
+++ break/2 sum upto 100.js
@@ -1,3 +1,13 @@
-/* 
-Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
-*/
+// Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
+
+let number = 1;
+let sum = 0;
+while (number <= 100) {
+  if (number > 100) {
+    break;
+  }
+  sum += number;
+  console.log(number);
+  console.log(number, "number", " total sum:", sum);
+  number++;
+}
