ce86de825ade41d87000e6aab2e38bf35a577780
siddikur-dev <siddikur.dev@gmail.com>
2025-07-30 16:47:26 +0600

    break/century.js solved

--- break/1 century.js
+++ break/1 century.js
@@ -1,5 +1,8 @@
-/* 
+// Write a loop 1 to 200. Use break to exit the loop once you find 100.
 
-Write a loop 1 to 200. Use break to exit the loop once you find 100.
-
-*/
+for (let number = 1; number < 200; number++) {
+  if (number === 101) {
+    break;
+  }
+  console.log(number);
+}
