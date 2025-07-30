ed355222ded036bf4beca8a544d5c218a570826f
siddikur-dev <siddikur.dev@gmail.com>
2025-07-30 18:24:59 +0600

    while/1 solved

--- while/1 commitment.js
+++ while/1 commitment.js
@@ -1,7 +1,10 @@
-/***
+// "I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
 
-"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
-
- */
-
-/*programming hero*/
+let number = 1;
+while (number <= 60) {
+  console.log(
+    number,
+    "I will invest at least 6 hrs every single day for next 60 days!"
+  );
+  number++;
+}
