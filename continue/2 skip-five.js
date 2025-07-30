2201b06daaf08acddeaaea59bc2ce8fff8c89fe8
siddikur-dev <siddikur.dev@gmail.com>
2025-07-30 17:54:06 +0600

    continue/2 skip-five.js solved

--- continue/2 skip-five.js
+++ continue/2 skip-five.js
@@ -1,3 +1,11 @@
 /*
 display odd number from 55 to 85 and skip the numbers divisible by 5.
 */
+
+for (let number = 55; number <= 85; number += 2) {
+  if (number % 5 === 0) {
+    
+    continue;
+  }
+  console.log(number);
+}
