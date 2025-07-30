4025620f1430c2729afaeb605698ff26e50464ab
siddikur-dev <siddikur.dev@gmail.com>
2025-07-30 18:21:16 +0600

    for/5 solved:

--- for/5 countDown.js
+++ for/5 countDown.js
@@ -1,8 +1,5 @@
-/***
+// Implement a countdown timer that counts down from 81 to 65.
 
-Implement a countdown timer that counts down from 81 to 65.
-
- */
-
-
-/*programming hero*/
+for (let i = 81; i >= 65; i--) {
+  console.log(i);
+}
