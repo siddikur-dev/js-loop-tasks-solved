79bcc1b8e722540d20a054791186252202d2b3b1
siddikur-dev <siddikur.dev@gmail.com>
2025-07-30 17:57:37 +0600

    for/1 solved'

--- for/1 commitment.js
+++ for/1 commitment.js
@@ -1,7 +1,7 @@
 /***
-
 "I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
-
  */
 
-/*programming hero*/
+for (let number=1;number<=60;number++){
+    console.log(number,"I will invest at least 6 hrs every single day for next 60 days!");
+}gi
