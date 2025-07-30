718636d257e531bb361cd4c711dc89c8d7ee677b
siddikur-dev <siddikur.dev@gmail.com>
2025-07-30 18:38:47 +0600

    while/3 solvedx

--- while/3 sum of odd numbers.js
+++ while/3 sum of odd numbers.js
@@ -1,18 +1,20 @@
-
-
-/***
-
-Subtask-1:
-
-Display sum of all the odd numbers from 81 to 131.
-
- */
-/***
-
-Subtask-2:
-
-Display sum of all the even numbers from 206 to 311.
-
- */
-
-/*programming hero*/
+// Display sum of all the odd numbers from 81 to 131.
+let number = 81;
+let sumOdd = 0;
+while (number <= 131) {
+  //   console.log(number);
+  sumOdd += number;
+  number += 2;
+}
+// console.log("sumOdd", sumOdd);
+// Display sum of all the even numbers from 206 to 311.
+let i = 206;
+let sumEven = 0;
+while (i <= 311) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+  sumEven += i;
+  i += 2;
+}
+console.log("sumEven", sumEven);
