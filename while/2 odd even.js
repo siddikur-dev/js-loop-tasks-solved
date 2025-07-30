2406127480e9fa68c96c9a3355fff035692bfc6b
siddikur-dev <siddikur.dev@gmail.com>
2025-07-30 18:30:53 +0600

    while/2 solved

--- while/2 odd even.js
+++ while/2 odd even.js
@@ -1,18 +1,15 @@
-/***
+// Find all the odd numbers from 61 to 100.
 
-Subtask-1:
+let number = 61;
+while (number <= 100) {
+  console.log(number);
+  number += 2;
+}
 
-Find all the odd numbers from 61 to 100.
+// Find all the even numbers from 78 to 98.
 
- */
-
-
-/***
-
-Subtask-2:
-
-Find all the even numbers from 78 to 98.
-
- */
-
-/*programming hero*/
+let i = 78;
+while (i <= 98) {
+  console.log(i);
+  i += 2;
+}
