916b4844f4c7981acc9bd6132b2aa294628be7e2
siddikur-dev <siddikur.dev@gmail.com>
2025-07-30 18:00:47 +0600

    for/2 solved

--- for/1 commitment.js
+++ for/1 commitment.js
@@ -2,6 +2,9 @@
 "I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
  */
 
-for (let number=1;number<=60;number++){
-    console.log(number,"I will invest at least 6 hrs every single day for next 60 days!");
-}gi
+for (let number = 1; number <= 60; number++) {
+  console.log(
+    number,
+    "I will invest at least 6 hrs every single day for next 60 days!"
+  );
+}
--- for/2 odd even.js
+++ for/2 odd even.js
@@ -1,18 +1,11 @@
-/***
+// Find all the odd numbers from 61 to 100.
+for (let number=61;number<=100;number+=2){
+    // console.log(number);
+}
 
-Subtask-1:
+// Find all the even numbers from 78 to 98.
 
-Find all the odd numbers from 61 to 100.
+for(let i=78;i<=98;i+=2){
+    console.log(i);
+}
 
- */
-
-
-/***
-
-Subtask-2:
-
-Find all the even numbers from 78 to 98.
-
- */
-
-/*programming hero*/
