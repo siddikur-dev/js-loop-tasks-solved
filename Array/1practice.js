933cf8c727cd2d552d36fefdb420b1c842930807
siddikur-dev <siddikur.dev@gmail.com>
2025-07-31 09:31:52 +0600

    array/1 solved

---
+++ Array/1practice.js
@@ -0,0 +1,12 @@
+/*
+1. Declare an array
+Declare an array with 5 elements containing fruits
+console log the 3rd index element
+change the value of the 2nd index element to jambura
+console log the final array
+*/
+
+const fruits = ["Apple", "Mango", "Banana", "Pineapple", "Guava"];
+console.log(fruits[3]);
+fruits[2]="Jambura"
+console.log(fruits);
