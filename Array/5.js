8dbecc692236ced0e4c6764290512895d43f5948
siddikur-dev <siddikur.dev@gmail.com>
2025-07-31 10:39:28 +0600

    array/5 solved:

---
+++ Array/5.js
@@ -0,0 +1,24 @@
+/**
+ *5. Combining Arrays
+Instructions:
+
+Create two arrays of your choice.
+Use the concat method to combine the two arrays into a new array.
+Print both the original arrays and the combined array using console.log().
+ * 
+ *  
+ * নিজের ইচ্ছামতো দুটি অ্যারে তৈরি করো।
+যেমন ধরো — array1 আর array2
+
+তারপর concat() মেথড ব্যবহার করে এই দুইটি অ্যারে একসাথে জয়েন করো একটা নতুন অ্যারেতে।
+
+শেষে, console.log() ব্যবহার করে —
+আর যে নতুন অ্যারে তৈরি হয়েছে — সবগুলো প্রিন্ট করো।
+
+
+ */
+const array1=['fatiha binte faruk']
+const array2=['binte fatiha ']
+console.log(array1);
+const concat=array1.concat(array2)
+console.log(concat);
